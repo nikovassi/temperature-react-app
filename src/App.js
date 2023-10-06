5fc27374a0f5b055a34f6929b04692ad514fd673
Nikolay Vasilev <94871781+nikovassi@users.noreply.github.com>
2023-10-06 08:16:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function App() {
+  const [tempValue, setTempValue] = useState(10);
+  const [tempColor, setTempColor] = useState("cold");
+
+  const increaseTemp = () => {
+    if(tempValue === 30) return;
+    const newTemp = tempValue + 1;
+
+    if (newTemp >= 15) {
+      setTempColor("hot");
+    }
+    setTempValue(newTemp);
+  };
+
+  const decreaseTemp = () => {
+    if(tempValue === 0) return;
+    const newTemp = tempValue - 1;
+
+    if (newTemp < 15) {
+      setTempColor("cold");
+    }
+    setTempValue(newTemp);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${tempColor}`}>{tempValue}°C</div>
+      </div>
+      <div className="button-container">
+        <button onClick={() => increaseTemp()}>+</button>
+        <button onClick={() => decreaseTemp()}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
